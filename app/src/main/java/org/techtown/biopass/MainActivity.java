bda3918ffc3cf4e6fbb384839df559f93056aadf
ssunhong <bebo007@kakao.com>
2022-06-08 09:56:38 +0900

    commit

--- app/src/main/java/org/techtown/biopass/MainActivity.java
+++ app/src/main/java/org/techtown/biopass/MainActivity.java
@@ -7,24 +7,62 @@ import static android.hardware.biometrics.BiometricManager.Authenticators.DEVICE
 import static com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import net.sf.ntru.encrypt.EncryptionParameters;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.pqc.crypto.mceliece.McElieceCipher;
+import org.bouncycastle.pqc.crypto.mceliece.McElieceKeyGenerationParameters;
+import org.bouncycastle.pqc.crypto.mceliece.McElieceKeyPairGenerator;
+import org.bouncycastle.pqc.crypto.mceliece.McElieceParameters;
+import org.bouncycastle.pqc.crypto.mceliece.McEliecePrivateKeyParameters;
+import org.bouncycastle.pqc.crypto.mceliece.McEliecePublicKeyParameters;
+import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,63 +70,119 @@ public class MainActivity extends AppCompatActivity {
     private Executor executor;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
-
+    private File pemFile;
+    private client c = new client();
 
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("path", getApplicationContext().getFilesDir().getPath() );
+        pemFile = new File(getApplicationContext().getFilesDir().getPath()+"public.pem");
+        Security.addProvider(new BouncyCastlePQCProvider());
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String ID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
 
-        executor = ContextCompat.getMainExecutor(this);
-        biometricPrompt = new BiometricPrompt(this,
-                executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode,
-                                              @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                Toast.makeText(getApplicationContext(),
-                        "ERROR", Toast.LENGTH_SHORT)
-                        .show();
+        String key = null;
+        if (checkPemFile()) {
+            try {
+                key = c.execute("getkey", ID).get();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
-
-            @Override
-            public void onAuthenticationSucceeded(
-                    @NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                startActivity(new Intent(MainActivity.this, QrCreateActivity.class));
+            try {
+                writePemFile(getPublicKey(key));
+            } catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
             }
+        }
 
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("지문 인증")
+                    .setSubtitle("기기에 등록된 지문을 이용하여 지문을 인증해주세요.")
+                    .setNegativeButtonText("취소")
+                    .setDeviceCredentialAllowed(false)
+                    .build();
+
+            Button biometricLoginButton = findViewById(R.id.buttonAuthWithFingerprint);
+            biometricLoginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    biometricPrompt.authenticate(promptInfo);
+                }
+            });
+
+            executor = ContextCompat.getMainExecutor(this);
+            biometricPrompt = new BiometricPrompt(this,
+                    executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode,
+                                                  @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(getApplicationContext(),
+                                    "ERROR", Toast.LENGTH_SHORT)
+                            .show();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(
+                        @NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+
+                    Date currentTime = new Date();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", java.util.Locale.getDefault());
+                    String getTime = simpleDateFormat.format(currentTime);
+
+                    Intent intent = new Intent(MainActivity.this, QrCreateActivity.class);
+                    intent.putExtra("Enc", ID );
+                    startActivity(new Intent(intent));
+                }
+
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            Button scanButton = findViewById(R.id.scanButton);
+            scanButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                }
+            });
+        }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
             }
-        });
-
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("지문 인증")
-                .setSubtitle("기기에 등록된 지문을 이용하여 지문을 인증해주세요.")
-                .setNegativeButtonText("취소")
-                .setDeviceCredentialAllowed(false)
-                .build();
-
-        Button biometricLoginButton = findViewById(R.id.buttonAuthWithFingerprint);
-        biometricLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                biometricPrompt.authenticate(promptInfo);
-            }
-        });
-
-        Button scanButton = findViewById(R.id.scanButton);
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
-                startActivity(intent);
-            }
-        });
+        }
     }
 
+    public boolean checkPemFile(){
+        if(pemFile.isFile())
+            return false;
+        else
+            return true;
+    }
+    private void writePemFile(Key key) throws IOException {
+        Pem pemFile = new Pem(key, "publicKey");
+        pemFile.write(getApplicationContext().getFilesDir().getPath()+"public.pem");
+    }
+    private Key getPublicKey(String key) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] b = Base64.decode(key, Base64.DEFAULT);
+        KeyFactory keyFactory = KeyFactory.getInstance("McEliece");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(b);
+        return keyFactory.generatePublic(keySpec);
+    }
 }
--- app/src/main/java/org/techtown/biopass/QrCreateActivity.java
+++ app/src/main/java/org/techtown/biopass/QrCreateActivity.java
@@ -4,12 +4,14 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -20,19 +22,42 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-import net.sf.ntru.encrypt.EncryptionKeyPair;
-import net.sf.ntru.encrypt.EncryptionParameters;
-import net.sf.ntru.encrypt.NtruEncrypt;
 
+import org.bouncycastle.asn1.mozilla.PublicKeyAndChallenge;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.pqc.crypto.mceliece.McElieceCipher;
+import org.bouncycastle.pqc.crypto.mceliece.McEliecePrivateKeyParameters;
+import org.bouncycastle.pqc.crypto.mceliece.McEliecePublicKeyParameters;
+import org.bouncycastle.pqc.jcajce.provider.mceliece.McElieceKeysToParams;
+import org.bouncycastle.pqc.jcajce.spec.McElieceKeyGenParameterSpec;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Hashtable;
 
 public class QrCreateActivity extends AppCompatActivity {
-
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -40,45 +65,83 @@ public class QrCreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_create);
         ImageView qr = findViewById(R.id.QRview);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("Enc");
 
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try{
-            Hashtable hints = new Hashtable();
-            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-
-            TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-            String ID = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-
-
-            Date currentTime = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", java.util.Locale.getDefault());
-            String getTime = simpleDateFormat.format(currentTime);
-
-            NtruEncrypt ntru = new NtruEncrypt(EncryptionParameters.APR2011_439_FAST);
-            EncryptionKeyPair kp = ntru.generateKeyPair();
-
-            byte[] enc = ntru.encrypt((getTime+ID).getBytes(), kp.getPublic());
-            Log.e("enc", new String(enc) );
-            String str1 = android.util.Base64.encodeToString(enc, android.util.Base64.NO_WRAP); // 일단 QR코드의 재료가 될 문자열
-            Log.e("str1", str1 );
-            byte[] decode = android.util.Base64.decode(str1, android.util.Base64.NO_WRAP); // base64디코딩 실험
-            byte[] dec = ntru.decrypt(decode, kp); // ntru 복호화 실험
-            Toast.makeText(this, new String(enc), Toast.LENGTH_SHORT).show(); // 복호화 성공을 알 수 있음
-
-
-            BitMatrix bitMatrix = multiFormatWriter.encode(str1, BarcodeFormat.QR_CODE, 600, 600);
+            test();
+            PublicKey pk = getPublicKey(getApplicationContext().getFilesDir().getPath()+"public.pem");
+            McElieceCipher EnCipheredText = new McElieceCipher();
+            McEliecePublicKeyParameters GPKP = (McEliecePublicKeyParameters) McElieceKeysToParams.generatePublicKeyParameter((PublicKey) pk);
+            EnCipheredText.init(true, GPKP);
+            byte[] ciphertextBytes = EnCipheredText.messageEncrypt(msg.getBytes());
+            BitMatrix bitMatrix = multiFormatWriter.encode(new String(ciphertextBytes), BarcodeFormat.QR_CODE, 600, 600);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
             Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             qr.setImageBitmap(bitmap);
         }catch (Exception e){
             e.printStackTrace();
         }
+        /*
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 finish();
             }
         }, 5000);
+        */
          Toast.makeText(this, "5초 뒤 QR코드가 종료됩니다.", Toast.LENGTH_LONG).show();
+    }
+
+    public PublicKey getPublicKey(String filename) throws Exception {
+        File f = new File(filename);
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] keyBytes = new byte[(int) f.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+
+        String temp = new String(keyBytes);
+        String publicKeyPEM = temp.replace("-----BEGIN publicKey-----", "");
+        publicKeyPEM = publicKeyPEM.replace(System.lineSeparator(), "");
+        publicKeyPEM = publicKeyPEM.replace("-----END publicKey-----", "");
+        Log.e("key", publicKeyPEM);
+        byte[] encoded = Base64.decode(publicKeyPEM, Base64.DEFAULT);
+        KeyFactory keyFactory = KeyFactory.getInstance("McEliece");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    public String Enc(String msg, PublicKey key) throws InvalidKeyException {
+        McElieceCipher EnCipheredText = new McElieceCipher();
+        McEliecePublicKeyParameters GPKP = (McEliecePublicKeyParameters) McElieceKeysToParams.generatePublicKeyParameter(key);
+        EnCipheredText.init(true, GPKP);
+        byte[] ciphertextBytes = EnCipheredText.messageEncrypt(msg.getBytes());
+        //Log.e("ciphertextBytes", new String(ciphertextBytes) );
+        return new String(ciphertextBytes);
+    }
+
+    public void test() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, InvalidKeyException, InvalidCipherTextException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("McEliece", "BCPQC");
+
+        McElieceKeyGenParameterSpec params = new McElieceKeyGenParameterSpec(9, 33);
+        kpg.initialize(params);
+
+        KeyPair mcelieceKp = kpg.generateKeyPair();
+        PublicKey publicKey = mcelieceKp.getPublic();
+        PrivateKey privateKey = mcelieceKp.getPrivate();
+
+        McElieceCipher EnCipheredText = new McElieceCipher();
+        McEliecePublicKeyParameters GPKP = (McEliecePublicKeyParameters) McElieceKeysToParams.generatePublicKeyParameter(publicKey);
+        EnCipheredText.init(true, GPKP);
+        byte[] ciphertextBytes = EnCipheredText.messageEncrypt("me".getBytes());
+
+        McEliecePrivateKeyParameters G =  (McEliecePrivateKeyParameters) McElieceKeysToParams.generatePrivateKeyParameter(privateKey);
+        EnCipheredText.init(false, G);
+        byte[] de = EnCipheredText.messageDecrypt(ciphertextBytes);
+
+        Log.e("test", new String(de));
+
     }
 }
